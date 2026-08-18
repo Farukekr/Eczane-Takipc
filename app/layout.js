d6edd5a76069ace5857cfd504ee753d31930fcc6
Farukekr <omerfarukeker23@gmail.com>
2026-08-18 18:47:57 +0300

    Create RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Eczane Takip',
+  description: 'Eczane Hasta ve Reçete Takip Sistemi',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  );
+}
